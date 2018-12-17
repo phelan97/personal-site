4f94dbdecce4fdde3d48828da3321eec17b3b5ec
Sean Phelan <seanphelan97@gmail.com>
2018-12-16 21:32:46 -0700

    add some images

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,8 @@ function Footer() {
         <li>GI</li>
         <li>LI</li>
       </ul>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/icomoon" title="Icomoon">Icomoon</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      <span>Photo by Evgeni Evgeniev on Unsplash</span>
     </footer>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Skills from '../components/skills';
 import {Helmet} from 'react-helmet';
 import hero from '../images/hero.jpg';
+import home from '../images/home.png';
 import background from '../images/background.jpg';
 import './index.css';
 
@@ -18,11 +20,14 @@ const IndexPage = () => (
 
     <main>
       <section id="intro">
+        <h1>Portfolio - Sean Phelan</h1>
+        <p>I'm a web developer experienced in server side and front end programming. I love trying out new technologies, writing maintainable code, and working with clean designs.</p>
+      </section>
+      <section id="about">
         <h2>About</h2>
+        <p></p>
       </section>
-      <section id="skills">
-        <h2>Skills</h2>
-      </section>
+      <Skills />
       <section id="projects">
         <h2>Projects</h2>
       </section>
