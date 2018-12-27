5da42032e19029b4e4eb1bf366660fc1227109ec
Sean Phelan <seanphelan97@gmail.com>
2018-12-27 01:17:12 -0700

    finished majority of card layout

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -4,18 +4,20 @@ import { checkPropTypes } from 'prop-types';
 
 // TODO: image alt text
 function ProjectCard(props) {
+  // TODO: Map icon data to a list and display it. Remove tech prop
+  // props.icons
+  const listData = props.tech.map(str => <li>{str}</li>);
   return (
     <div className="project-card">
-      <h3>Title</h3>
-      <img src={props.projectImage} />
+      <h3>{props.title}</h3>
+      <img src={props.image} />
       <div className="project-info">
-        <p>Text</p>
+        <p>{props.desc}</p>
         <ul>
-          <li>a</li>
-          <li>b</li>
+          {listData}
         </ul>
         <div className="project-links">
-          <button>Source</button>
+          <button>GitHub</button>
           <button>Demo</button>
         </div>
       </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -4,12 +4,22 @@ import temp from '../images/background.jpg';
 import './projects.css';
 
 function Projects() {
+  // TODO: tech prop is temporary... switch to icons asap
+  let icons = {
+    // icon:
+    // alt:
+  }
+  let tech = ['React', 'Redux', 'Node.js'];
   return (
     <section class="projects-section">
       <a class="anchor" id="projects">&nbsp;</a>
       <h2>Projects</h2>
-      <ProjectCard projectImage={temp} />
-      <ProjectCard projectImage={temp} />
+      <ProjectCard image={temp} desc="project description" title="project 1"
+        icons={icons} tech={tech}/>
+      <ProjectCard image={temp} desc="project description" title="project 2"
+        icons={icons} tech={tech} />
+      <ProjectCard image={temp} desc="project description" title="project 2"
+        icons={icons} tech={tech} />
     </section>
   );
 }
