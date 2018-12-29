acdb2f60f06213cbd2517658e38cc7a9be550d2f
Sean Phelan <seanphelan97@gmail.com>
2018-12-28 18:52:23 -0700

    update about

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ function About() {
     <section className="about-section">
       <a class="anchor" id="about">&nbsp;</a>
       <h2>About Me</h2>
-      <p>I've always been interested in software and the "magic" behind the scenes that makes the complicated software that we rely on every day possible. I've been programming on and off since I was about 13, starting with Java. Unfortunately Javascript wasn't so hot back then, but I definitely learned a lot about programming! My interest in web development is more recent, and it's been an incredible experience to watch ideas come to life and evolve through the development process. When I'm not creating software, I'm frequently found reading or getting into spirited debates. I enjoy gaming, building things, and this strange place called "the great outdoors." </p>
+      <p>I've always been interested in software and the magic behind the scenes that makes the complicated software that we rely on every day possible. I've been programming on and off since I was about 13, starting with Java. Unfortunately Javascript wasn't so hot back then, but I definitely learned a lot about programming! My interest in web development is more recent, and it's been an incredible experience to watch ideas come to life and evolve through the development process. When I'm not creating software, I'm frequently found reading or getting into spirited debates. I also enjoy gaming, building things, and this strange place called "the great outdoors." </p>
     </section>
   )
 }
