3634d91af63a5de9bde1f14459e30d1d088380c3
Sean Phelan <seanphelan97@gmail.com>
2019-01-02 15:04:31 -0700

    intro and skills cleanup

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,7 +7,6 @@ function Intro() {
   return (
     <section class="intro-section">
       <a class="anchor" id="intro">&nbsp;</a>
-      {/* <h1>Portfolio - Sean Phelan</h1> */}
       <div id="text-block">
         <h1>Sean Phelan</h1>
         <p>I'm a web developer experienced with server side and front end programming. I love trying out new technologies, writing maintainable code, and working with clean design.</p>
