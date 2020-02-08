940b162f6d81a8fcec18ebab50ad0fc9ce7b0d09
Sean Phelan <seanphelan97@gmail.com>
2020-02-08 09:09:20 -0700

    partial menu and about implementation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import {StaticQuery, graphql} from 'gatsby'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import './navbar.css';
 
+// TODO: support ordering of elements... ordering is currently unspecified
 function NavBar() {
   return (
-    <nav role="nav">
-      <ul>
-        <li className="nav-projects"><AnchorLink href="#projects" offset="58px">Projects</AnchorLink></li>
-        <li className="nav-skills"><AnchorLink href="#skills" offset="58px">Skills</AnchorLink></li>
-        <li className="nav-about"><AnchorLink href="#about" offset="58px">About</AnchorLink></li>
-        <li className="nav-contact"><AnchorLink href="#contact" offset="58px">Contact</AnchorLink></li>
-        {/* <li><Link to="#">Resume</Link></li>
-        <li><Link to="#">LI</Link></li>
-        <li><Link to="#">GH</Link></li> */}
-      </ul>
-    </nav>
+    <StaticQuery query={navQuery} render={data => {
+      return (
+        <nav role="nav">
+          <ul>
+            {data.allContentfulNavigationEntry.edges.map((edge, index) => (
+              <li key={index}><AnchorLink key={edge.node.id} href={edge.node.partialUrl} offset="58px">{edge.node.title}</AnchorLink></li>
+            ))}
+          </ul>
+        </nav>
+      )
+    }}/>
+    
+    // <li className="nav-projects"><AnchorLink href="#projects" offset="58px">Projects</AnchorLink></li>
+    // <li className="nav-skills"><AnchorLink href="#skills" offset="58px">Skills</AnchorLink></li>
+    // <li className="nav-about"><AnchorLink href="#about" offset="58px">About</AnchorLink></li>
+    // <li className="nav-contact"><AnchorLink href="#contact" offset="58px">Contact</AnchorLink></li>
+    // {/* <li><Link to="#">Resume</Link></li>
+    // <li><Link to="#">LI</Link></li>
+    // <li><Link to="#">GH</Link></li> */}
   )
 }
+
+const navQuery = graphql`
+  {
+    allContentfulNavigationEntry {
+      edges {
+        node {
+          id
+          partialUrl
+          title
+        }
+      }
+    }
+  }
+`
 
 export default NavBar;
