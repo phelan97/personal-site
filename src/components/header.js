dd1a5806c79710724c3c8a8f482d045443491b06
Sean Phelan <seanphelan97@gmail.com>
2020-02-09 20:28:24 -0700

    minor style changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Link} from 'gatsby';
-import home from '../images/home.svg';
 import NavBar from '../components/navbar';
 
 import './header.css';
@@ -8,10 +6,6 @@ import './header.css';
 function Header() {
   return (
     <header className="header" role="banner">
-      <div id="logo">
-        <Link to="/"><img src={home} /></Link>
-      </div>
-
       <NavBar />
     </header>
   )
