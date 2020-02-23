a310d2405d340644ffdd96b6e1716ea715b09fc6
Sean Phelan <seanphelan97@gmail.com>
2020-02-22 21:49:25 -0700

    css reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,16 @@ import {Helmet} from 'react-helmet';
 import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
 import favicon from '../images/favicon.ico';
 import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset';
 import './index.css';
 
 const GlobalStyle = createGlobalStyle`
+${reset}
 @import url('https://fonts.googleapis.com/css?family=Open+Sans|Lato:400,700');
 * {
   box-sizing: border-box;
 }
 body {
-  margin: 0;
   font-family: 'Open Sans', sans-serif;
   font-size: 18px;
 }
@@ -28,6 +29,8 @@ h1, h2, h3, h4, h5, h6 {
 
 const IndexPage = () => (
   <React.Fragment>
+
+    <GlobalStyle />
 
     <Helmet>
       <meta charSet="utf-8" />
